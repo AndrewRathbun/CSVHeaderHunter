2b3b81063984d85177bdd3c61dd395839f714f1d
Andrew Rathbun <36825567+AndrewRathbun@users.noreply.github.com>
2022-03-04 23:57:58 -0500

    add documentation to look at later

--- CSVHeaderHunter/Program.cs
+++ CSVHeaderHunter/Program.cs
@@ -6,6 +6,9 @@ namespace CSVHeaderHunter // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            // https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-get-a-file-extension-in-C-Sharp/#:~:text=C%23%20Get%20File%20Extension.%20The%20Extension%20property%20of,extn%20%3D%20fi.Extension%3B%20Console.WriteLine%20%28%22File%20Extension%3A%20%7B0%7D%22%2C%20extn%29%3B
+            // https://docs.microsoft.com/en-us/dotnet/api/system.io.directory.getfiles?view=net-6.0
+            
             
         }
     }
