647f19ff2fba64ffaa75002c031e40c2cb20c205
Andrew Rathbun <36825567+AndrewRathbun@users.noreply.github.com>
2022-03-06 16:46:04 -0500

    362022

--- CSVHeaderHunter/Program.cs
+++ CSVHeaderHunter/Program.cs
@@ -1,52 +1,221 @@
-﻿class Program
+﻿using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using System.Globalization;
+using System.Reflection;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+
+namespace CsvHeaderHunter;
+
+internal class Program
 {
+    private static string _activeDateTimeFormat;
+    private static RootCommand _rootCommand;
+    private static readonly string BaseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+    private static readonly string Header =
+        $"CSVHeaderHunter version {Assembly.GetExecutingAssembly().GetName().Version}" +
+        "\r\n\r\nAuthor: Andrew Rathbun (andrew.d.rathbun@gmail.com)" +
+        "\r\nhttps://github.com/AndrewRathbun/CSVHeaderHunter";
+
+    private static readonly string Footer =
+        @"Examples: CSVHeaderHunter.exe -d ""C:\"" " +
+        "\r\n\t " +
+        "    Short options (single letter) are prefixed with a single dash. Long commands are prefixed with two dashes";
+
+    private static async Task Main(string[] args)
+    {
+        _rootCommand = new RootCommand
+        {
+            new Option<string>(
+                "-d",
+                "Directory to process recursively that contains CSV files."),
+            /*new Option<string>(
+                "--csv",
+                "Directory to save CSV formatted results to"),
+            new Option<bool>(
+                "--debug",
+                () => false,
+                "Show debug information during processing"),
+            new Option<bool>(
+                "--trace",
+                () => false,
+                "Show trace information during processing")*/
+        };
+
+        _rootCommand.Description = Header + "\r\n\r\n" + Footer;
+
+        _rootCommand.Handler = CommandHandler.Create(DoWork);
+
+        await _rootCommand.InvokeAsync(args);
+
+        Log.CloseAndFlush();
+    }
+
+
+    /*
+    /*
     static void Main(string[] args)
     {
         //.CSV: comma seperated values
-        AppendToCSV();
-        ReadCSVFile();
+        ReadCSVFileHeaders();
+
+
+        // AppendToCSV();
+
+
         Console.ReadLine();
     }
-    
+    #1#
     // https://crudzone.wordpress.com/wpf/
-    static void AppendToCSV()
+
+    /*static void AppendToCSV()
     {
         var list = Contacts.GetContacts();
         foreach (var c in list)
         {
             File.AppendAllText("contacts.csv", $"{c.Name},{c.Phone}\n");
         }
-    }
-    static void ReadCSVFile()
+    }#1#
+    static void ReadCSVFileHeaders()
     {
-        var lines = File.ReadAllLines("C:\\Users\\CFUser\\OneDrive - Kroll\\CyberVM\\EDrive\\Training\\Directory Opus Demo\\Flat View Filtering Demo - Canon KAPE Output, also ctrl f demo\\KapeTriage_L-114213\\EventLogs\\20200919142304_EvtxECmd_Output.csv");
-        var list = new List<Contact>();
+        var lines = File.ReadAllLines(
+            "d", d;
+        var list = new List<CsvFiles>();
         foreach (var line in lines)
         {
             var values = line.Split(',');
-            if (values.Length==2)
+            if (values.Length == 2)
             {
-                var contact = new Contact() { Name = values[0], Phone = values[1] };
-                list.Add(contact); 
+                var csvFiles = new CsvFiles();
+                list.Add(csvFiles);
             }
         }
-        list.ForEach(x => Console.WriteLine($"{x.Name}\t{x.Phone}"));
     }
-} 
-public class Contacts
-{
-    public static List<CsvHeaders> GetContacts()
+    */
+
+    private static void DoWork(string f, string d, string csv, bool debug, bool trace)
     {
-        return new List<CsvHeaders>()
+        /*
+        var levelSwitch = new LoggingLevelSwitch();
+
+        _activeDateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+
+        var formatter =
+            new DateTimeOffsetFormatter(CultureInfo.CurrentCulture);
+
+        var template = "{Message:lj}{NewLine}{Exception}";
+
+        
+        if (debug)
         {
-            new CsvHeaders(){Name="Jill", Phone="333-444-5555"},
-            new CsvHeaders(){Name="Jane", Phone="669-444-7777"},
-            new CsvHeaders(){Name="Hill", Phone="222-444-8888"},
-        };
+            levelSwitch.MinimumLevel = LogEventLevel.Debug;
+            template = "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+        }
+
+        if (trace)
+        {
+            levelSwitch.MinimumLevel = LogEventLevel.Verbose;
+            template = "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+        }
+
+        if (Directory.Exists(csv) == false)
+        {
+            Log.Information("Path to {Csv} doesn't exist. Creating...", csv);
+
+            try
+            {
+                Directory.CreateDirectory(csv);
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Unable to create directory {Csv}. Does a file with the same name exist? Exiting",
+                    csv);
+                Console.WriteLine();
+                return;
+            }
+            */
+
+        if (string.IsNullOrEmpty(d) == false)
+        {
+            if (Directory.Exists(d) == false)
+            {
+                Log.Information("Looking for CSV files in {D}", d);
+                Console.WriteLine();
+            }
+
+            ProcessFile(Path.GetFullPath(d), csv);
+        }
+        else
+        {
+            //Directories
+
+            var files = new List<string>();
+
+            var enumerationOptions = new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                MatchCasing = MatchCasing.CaseSensitive,
+                RecurseSubdirectories = true,
+                AttributesToSkip = 0x0
+            };
+
+            IEnumerable<string> files2;
+
+            files2 = Directory.EnumerateFileSystemEntries(d, "*.csv", enumerationOptions);
+            
+            foreach (var file in files)
+            {
+                try
+                {
+                    ProcessFile(file, csv);
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "Error processing {File}: {Message}", file, e.Message);
+                }
+            }
+        }
+    }
+    private class DateTimeOffsetFormatter : IFormatProvider, ICustomFormatter
+    {
+        private readonly IFormatProvider _innerFormatProvider;
+
+        public DateTimeOffsetFormatter(IFormatProvider innerFormatProvider)
+        {
+            _innerFormatProvider = innerFormatProvider;
+        }
+
+        public object? GetFormat(Type? formatType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private static void ProcessFile(string getFullPath, string csv)
+    {
+        Console.WriteLine("test");
+        throw new NotImplementedException();
     }
 }
-public class CsvHeaders
+
+
+
+
+internal class ApplicationArguments
 {
-    public string FileName { get; set; }
-    public string Headers { get; set; }
+    public string File { get; set;}
+    public string Directory { get; set;}
+    /*
+    public string CsvDirectory { get; set; }
+    public bool Debug { get; set; }
+    public bool Trace { get; set; }
+*/
 }
