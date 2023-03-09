9c8452b9648aa3f6c7cde77e747dfeb9adb90b67
Andrew Rathbun <36825567+AndrewRathbun@users.noreply.github.com>
2023-03-08 21:47:48 -0500

    add input and output path

--- CSVHeaderHunter/CSVHeaderHunter/Program.cs
+++ CSVHeaderHunter/CSVHeaderHunter/Program.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Get the path of the folder from the user
-        Console.WriteLine("Enter the path of the folder:");
-        string folderPath = Console.ReadLine();
+        string folderPath;
+        string outputPath;
+
+        // Check if the user has specified a folder path as a command-line argument
+        if (args.Length > 0)
+        {
+            folderPath = args[0];
+        }
+        else
+        {
+            // Prompt the user to enter the folder path
+            Console.WriteLine("Enter the path of the folder:");
+            folderPath = Console.ReadLine();
+        }
+
+        // Check if the user has specified an output path as a command-line argument
+        if (args.Length > 1)
+        {
+            outputPath = args[1];
+        }
+        else
+        {
+            // Use the input folder as the default output path
+            outputPath = folderPath;
+        }
 
         // Get all CSV files in the folder and its subfolders
         var csvFiles = Directory.GetFiles(folderPath, "*.csv", SearchOption.AllDirectories);
 
         // Create a new text file to write the first lines of the CSV files
-        string outputFile = Path.Combine(folderPath, "output.txt");
+        string outputFile = Path.Combine(outputPath, "output.txt");
         using (StreamWriter writer = new StreamWriter(outputFile))
         {
             // Write the first line of each CSV file to the text file
